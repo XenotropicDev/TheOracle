23943a408dbbf32decb5d66dfa31333afd2c589b
Dominic Eliot <eliotd@gmail.com>
2020-09-23 11:20:43 -0700

    Adds support for starforged planet rolls

--- TheOracle/StarForged/Planet.cs
+++ TheOracle/StarForged/Planet.cs
@@ -1,130 +1,59 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TheOracle.Core;
 
 namespace TheOracle.StarForged
 {
     public class Planet
     {
+        public int Seed { get; set; }
+        public static Planet GeneratePlanet(string planetName)
+        {
+            var p = new Planet();
+
+            p.Seed = planetName.GetDeterministicHashCode();
+            Random PlanetRandom = new Random(p.Seed);
+
+            PlanetTemplate Template = PlanetTemplate.GetPlanetTemplates().GetRandomRow(PlanetRandom);
+
+            p.Atmosphere = Template.Atmospheres.GetRandomRow(PlanetRandom);
+            p.Biomes = Biome.GetFromTemplate(Template, PlanetRandom);
+            
+            p.CloserLooks = Template.CloserLooks;
+            p.CloserLooks.Shuffle(PlanetRandom);
+
+            p.Description = Template.Description;
+            p.Life = Template.PossibleLife.GetRandomRow(PlanetRandom);
+            p.Name = planetName;
+            p.PlanetType = Template.PlanetType;
+            p.Settlements.Terminus = Template.PossibleSettlements.Terminus.GetRandomRow(PlanetRandom);
+            p.Settlements.Outlands = Template.PossibleSettlements.Outlands.GetRandomRow(PlanetRandom);
+            p.Settlements.Expanse = Template.PossibleSettlements.Expanse.GetRandomRow(PlanetRandom);
+            p.Thumbnail = Template.Thumbnail;
+            
+            var so = new List<SpaceObservation>(Template.SpaceObservations);
+            so.Shuffle(PlanetRandom);
+
+            p.SpaceObservations = new List<SpaceObservation>(so.Take(PlanetRandom.Next(1,3)));
+
+            return p;
+        }
+
+        public Planet()
+        {
+        }
+
         public string Name { get; set; }
+        public string PlanetType { get; set; }
         public string Description { get; set; }
-        public List<Atmosphere> Atmospheres { get; set; }
-        public List<Settlement> Settlements { get; set; }
-        public int MaxSpaceObservations { get; set; }
-        public List<Spaceobservation> SpaceObservations { get; set; }
-        public List<Numberofbiomes> NumberOfBiomes { get; set; }
-        public List<Biome> PossibleBiomes { get; set; }
-        public List<Life> PossibleLife { get; set; }
-        public List<Closerlook> CloserLooks { get; set; }
-    }
-
-    public class Atmosphere
-    {
-        public int Chance { get; set; }
-        public string Description { get; set; }
-    }
-
-    public class Settlement
-    {
-        public Terminus Terminus { get; set; }
-        public Outlands Outlands { get; set; }
-        public Expanse Expanse { get; set; }
-    }
-
-    public class Terminus
-    {
-        public string _30 { get; set; }
-        public string _40 { get; set; }
-        public string _75 { get; set; }
-        public string _90 { get; set; }
-        public string _100 { get; set; }
-    }
-
-    public class Outlands
-    {
-        public string _60 { get; set; }
-        public string _65 { get; set; }
-        public string _90 { get; set; }
-        public string _97 { get; set; }
-        public string _100 { get; set; }
-    }
-
-    public class Expanse
-    {
-        public string _80 { get; set; }
-        public string _83 { get; set; }
-        public string _93 { get; set; }
-        public string _98 { get; set; }
-        public string _100 { get; set; }
-    }
-
-    public class Spaceobservation
-    {
-        public string _11 { get; set; }
-        public string _22 { get; set; }
-        public string _33 { get; set; }
-        public string _44 { get; set; }
-        public string _55 { get; set; }
-        public string _66 { get; set; }
-        public string _77 { get; set; }
-        public string _88 { get; set; }
-        public string _92 { get; set; }
-        public string _100 { get; set; }
-    }
-
-    public class Numberofbiomes
-    {
-        public int _20 { get; set; }
-        public int _70 { get; set; }
-        public int _90 { get; set; }
-        public int _100 { get; set; }
-    }
-
-    public class Biome
-    {
-        public string _5 { get; set; }
-        public string _10 { get; set; }
-        public string _15 { get; set; }
-        public string _20 { get; set; }
-        public string _25 { get; set; }
-        public string _30 { get; set; }
-        public string _35 { get; set; }
-        public string _40 { get; set; }
-        public string _45 { get; set; }
-        public string _50 { get; set; }
-        public string _55 { get; set; }
-        public string _60 { get; set; }
-        public string _65 { get; set; }
-        public string _70 { get; set; }
-        public string _75 { get; set; }
-        public string _80 { get; set; }
-        public string _85 { get; set; }
-        public string _90 { get; set; }
-        public string _95 { get; set; }
-        public string _100 { get; set; }
-    }
-
-    public class Life
-    {
-        public string _20 { get; set; }
-        public string _50 { get; set; }
-        public string _85 { get; set; }
-        public string _100 { get; set; }
-    }
-
-    public class Closerlook
-    {
-        public string _6 { get; set; }
-        public string _13 { get; set; }
-        public string _20 { get; set; }
-        public string _26 { get; set; }
-        public string _32 { get; set; }
-        public string _38 { get; set; }
-        public string _44 { get; set; }
-        public string _51 { get; set; }
-        public string _57 { get; set; }
-        public string _64 { get; set; }
-        public string _71 { get; set; }
-        public string _78 { get; set; }
-        public string _88 { get; set; }
-        public string _100 { get; set; }
+        public string Thumbnail { get; set; }
+        public Atmosphere Atmosphere { get; set; } = new Atmosphere();
+        public Settlements Settlements { get; set; } = new Settlements();
+        public List<SpaceObservation> SpaceObservations { get; set; } = new List<SpaceObservation>();
+        public int NumberOfBiomes { get => Biomes?.Count ?? 0; }
+        public List<Biome> Biomes { get; set; } = new List<Biome>();
+        public Life Life { get; set; }
+        public List<CloserLook> CloserLooks { get; set; } = new List<CloserLook>();
     }
 }
---
+++ TheOracle/StarForged/PlanetTemplate.cs
@@ -0,0 +1,154 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using TheOracle.Core;
+
+namespace TheOracle.StarForged
+{
+    public class PlanetTemplate : IOracleChance
+    {
+        public string PlanetType { get; set; }
+        public int Chance { get; set; }
+        public string Description { get; set; }
+        public string Thumbnail { get; set; }
+        public List<Atmosphere> Atmospheres { get; set; } = new List<Atmosphere>();
+        public PossibleSettlements PossibleSettlements { get; set; } = new PossibleSettlements();
+        public int MaxSpaceObservations { get; set; }
+        public List<SpaceObservation> SpaceObservations { get; set; } = new List<SpaceObservation>();
+        public List<Numberofbiomes> NumberOfBiomes { get; set; } = new List<Numberofbiomes>();
+        public List<Biome> PossibleBiomes { get; set; } = new List<Biome>();
+        public List<Life> PossibleLife { get; set; } = new List<Life>();
+        public List<CloserLook> CloserLooks { get; set; } = new List<CloserLook>();
+
+        public static List<PlanetTemplate> GetPlanetTemplates()
+        {
+            var cache = BotCache.Get();
+                
+            var cachedPlanets = cache.GetOrCreate("PlanetTemplates", entry =>
+            {
+                entry.SlidingExpiration = TimeSpan.FromSeconds(10);
+                string json = File.ReadAllText("StarForged\\MultiplePlanets.json");
+                return JsonConvert.DeserializeObject<List<PlanetTemplate>>(json);
+            });
+
+            return cachedPlanets;
+        }
+    }
+
+    public class Atmosphere : IOracleChance
+    {
+        public int Chance { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class PossibleSettlements
+    {
+        public List<Terminus> Terminus { get; set; } = new List<Terminus>();
+        public List<Outlands> Outlands { get; set; } = new List<Outlands>();
+        public List<Expanse> Expanse { get; set; } = new List<Expanse>();
+    }
+
+    public class Settlements
+    {
+        public Terminus Terminus { get; set; }
+        public Outlands Outlands { get; set; }
+        public Expanse Expanse { get; set; }
+    }
+    
+
+    public class Terminus : IOracleChance
+    {
+        public int Chance { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class Outlands : IOracleChance
+    {
+        public int Chance { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class Expanse : IOracleChance
+    {
+        public int Chance { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class SpaceObservation : IOracleChance
+    {
+        public int Chance { get; set; }
+        public string Description { get; set; }
+
+    }
+
+    public class Numberofbiomes
+    {
+        public int Chance { get; set; }
+        public int Amount { get; set; }
+    }
+
+    public class Biome : IOracleChance
+    {
+        public int Chance { get; set; }
+        public string Description { get; set; }
+
+        internal static List<Biome> GetFromTemplate(PlanetTemplate template, Random planetRandom = null)
+        {
+            if (planetRandom == null) planetRandom = BotRandom.Instance;
+            List<Biome> Biomes = new List<Biome>();
+            int BiomesToGenerate = template.NumberOfBiomes.OrderBy(b => b.Chance).First(b => b.Chance >= planetRandom.Next(1, 100)).Amount;
+
+
+            if (template.PossibleBiomes.Count <= 1 || BiomesToGenerate <= template.PossibleBiomes.Count)
+            {
+                Biomes.AddRange(template.PossibleBiomes);
+                return Biomes;
+            }
+            
+            if (BiomesToGenerate > 0)
+            {
+                for (int i = 1; i <= BiomesToGenerate; i++)
+                {
+                    Biome biome;
+                    do
+                    {
+                        biome = template.PossibleBiomes.GetRandomRow(planetRandom);
+                    } while (Biomes.Any(b => b.Chance == biome.Chance)); //don't add the same biomes more than once.
+
+                    Biomes.Add(biome);
+                }
+            }
+            return Biomes;
+        }
+    }
+
+    public class Life : IOracleChance
+    {
+        public int Chance { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class CloserLook : IOracleChance
+    {
+        public int Chance { get; set; }
+        public string Description { get; set; }
+
+        //internal static List<CloserLook> GetFromTemplate(PlanetTemplate template, Random planetRandom = null)
+        //{
+        //    if (planetRandom == null) planetRandom = BotRandom.Instance;
+        //    List<CloserLook> Looks = new List<CloserLook>();
+        //    int BiomesToGenerate = template.CloserLooks.GetRandomRow(planetRandom);
+
+
+        //    if (template.PossibleBiomes.Count <= 1 || BiomesToGenerate <= template.PossibleBiomes.Count)
+        //    {
+        //        Looks.AddRange(template.PossibleBiomes);
+        //        return Looks;
+        //    }
+        //}
+    }
+}
