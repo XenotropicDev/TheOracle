bae9709f8f46439d44e3a3079003268f77d16266
Dominic Eliot <eliotd@gmail.com>
2020-09-23 11:20:20 -0700

    Unit test project

---
+++ TheOracleTests/StarForged/PlanetGenCommandTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TheOracle.IronSworn;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheOracle.IronSworn.Tests
+{
+    [TestClass()]
+    public class PlanetGenCommandTests
+    {
+        [TestMethod()]
+        public void GeneratePlanetTest()
+        {
+            var p = new PlanetCommands().GeneratePlanet();
+        }
+    }
+}
---
+++ TheOracleTests/StarForged/PlanetTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TheOracle.StarForged;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheOracle.StarForged.Tests
+{
+    [TestClass()]
+    public class PlanetTests
+    {
+        [TestMethod()]
+        public void GeneratePlanetTest()
+        {
+            var p1 = Planet.GeneratePlanet("Test");
+
+            Assert.IsTrue(p1.Atmosphere != default);
+
+            Assert.AreEqual(-1701808026, p1.Seed);
+        }
+    }
+}
