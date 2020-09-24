81c7d278d62388bd4ba6731d0d460e554fce02e5
Dominic Eliot <eliotd@gmail.com>
2020-09-23 20:44:49 -0700

    fix for oracle list being added to multiple times

--- TheOracle/IronSworn/OracleService.cs
+++ TheOracle/IronSworn/OracleService.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace TheOracle.Core
@@ -11,6 +13,10 @@ namespace TheOracle.Core
         public OracleService()
         {
             OracleList = new List<OracleTable>();
+            
+            //TODO change this to load more dynamically?
+            var oracleData = JsonConvert.DeserializeObject<List<OracleTable>>(File.ReadAllText("IronSworn\\oracles.json"));
+            OracleList.AddRange(oracleData);
         }
     }
 }
