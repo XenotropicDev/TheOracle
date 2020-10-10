d8fe1799e69a221076130883344f26b0ec130240
Dominic Eliot <eliotd@gmail.com>
2020-10-09 18:12:08 -0700

    Added a localization safe percentage parser

---
+++ TheOracle/BotCore/Utilities.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace TheOracle.BotCore
+{
+    class Utilities
+    {
+        public static decimal ConvertPercentToDecimal(string percentValue, CultureInfo culture = default)
+        {
+            if (culture == default) culture = CultureInfo.CurrentCulture;
+            
+            var numFormat = culture.NumberFormat;
+
+            NumberFormatInfo nfi = new NumberFormatInfo()
+            {
+                CurrencyDecimalDigits = numFormat.PercentDecimalDigits,
+                CurrencyDecimalSeparator = numFormat.PercentDecimalSeparator,
+                CurrencyGroupSeparator = numFormat.PercentGroupSeparator,
+                CurrencyGroupSizes = numFormat.PercentGroupSizes,
+                CurrencyNegativePattern = numFormat.PercentNegativePattern,
+                CurrencyPositivePattern = numFormat.PercentPositivePattern,
+                CurrencySymbol = numFormat.PercentSymbol
+            };
+
+            var convertedValue = decimal.Parse(percentValue, NumberStyles.Currency, nfi);
+            return convertedValue / 100m;
+        }
+    }
+}
