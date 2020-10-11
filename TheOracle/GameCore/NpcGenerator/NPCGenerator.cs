40b5dfc1fbcd27167810448d6f60044308192086
Dominic Eliot <eliotd@gmail.com>
2020-10-10 17:45:48 -0700

    Basic support for NPC generation

---
+++ TheOracle/GameCore/NpcGenerator/NPCGenerator.cs
@@ -0,0 +1,15 @@
+﻿using Discord;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheOracle.GameCore.NpcGenerator
+{
+    public abstract class NPCGenerator
+    {
+        public virtual Embed GetEmbed()
+        {
+            return new EmbedBuilder().Build();
+        }
+    }
+}
---
+++ TheOracle/GameCore/NpcGenerator/NpcCommands.cs
@@ -0,0 +1,42 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+using TheOracle.Core;
+using TheOracle.IronSworn;
+
+namespace TheOracle.GameCore.NpcGenerator
+{
+    public class NpcCommands : ModuleBase<SocketCommandContext>
+    {
+        public NpcCommands(DiscordSocketClient client, OracleService oracleService)
+        {
+            //Add client via DI, needed for reaction events
+            Client = client;
+            OracleService = oracleService;
+        }
+
+        public DiscordSocketClient Client { get; }
+        public OracleService OracleService { get; }
+
+        [Command("CreateNPC")]
+        [Summary("Creates a NPC with a role, goal, and descriptor")]
+        [Alias("NewNPC", "NPC")]
+        public async Task CreateNPC(string NPCCreationOptions = "")
+        {
+            var game = GameName.Ironsworn;
+
+            NPCGenerator npcGen = null;
+
+            if (game == GameName.Ironsworn)
+            {
+                var npc = new IronNPC(OracleService, NPCCreationOptions) { };
+
+                npcGen = npc;
+            }
+
+            if (npcGen != null) await ReplyAsync(embed: npcGen.GetEmbed());
+
+            return;
+        }
+    }
+}
