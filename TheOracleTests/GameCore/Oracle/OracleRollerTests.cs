253c89a43745f7410c5d28f0ab5f15acd06cb28d
Dominic Eliot <eliotd@gmail.com>
2020-10-15 10:59:45 -0700

    additional action and oracle unit test

---
+++ TheOracleTests/GameCore/Oracle/OracleRollerTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TheOracle.Core;
+
+namespace TheOracle.GameCore.Oracle.Tests
+{
+    [TestClass()]
+    public class OracleRollerTests
+    {
+        [TestMethod()]
+        [TestCategory("Integration")]
+        public void BuildRollResultsTest()
+        {
+            var oracleService = new OracleService();
+
+            oracleService.RandomRow("Action", GameName.Ironsworn);
+            oracleService.RandomRow("Theme", GameName.Ironsworn);
+
+            Assert.ThrowsException<ArgumentException>(() => oracleService.RandomRow("Action"));
+        }
+    }
+}
