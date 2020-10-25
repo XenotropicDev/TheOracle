e023f02b305b4c4c776fc94fae805ccf124b417e
Dominic Eliot <eliotd@gmail.com>
2020-10-25 12:43:47 -0700

    Fix for spamming moved message to bottom messages.

--- TheOracle/BotCore/GlobalReactionHandler.cs
+++ TheOracle/BotCore/GlobalReactionHandler.cs
@@ -1,8 +1,10 @@
 ﻿using Discord;
 using Discord.WebSocket;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
+using TheOracle.GameCore.Oracle;
 
 namespace TheOracle.BotCore
 {
@@ -27,17 +29,26 @@ namespace TheOracle.BotCore
             {
                 _ = Task.Run(async () =>
                 {
-                    var warningMsg = await channel.SendMessageAsync($"{reaction.User} moved the following message to the bottom of chat from a message posted on {message.Timestamp.ToLocalTime()}");
+                    if (message.Embeds.Count == 0) return;
+
+                    if (message.Embeds.Any(NeedsWarning)) await channel.SendMessageAsync($"{reaction.User} moved the following message to the bottom of chat from a message posted on {message.Timestamp.ToLocalTime()}");
 
                     var reactionsToAdd = message.Reactions.Where(item => item.Value.IsMe).Select(item => item.Key);
-                    var msg = await channel.SendMessageAsync(message.Content, message.IsTTS, message.Embeds.FirstOrDefault() as Embed);
+                    var newMessage = await channel.SendMessageAsync(message.Content, message.IsTTS, message.Embeds.FirstOrDefault() as Embed);
 
-                    await msg.AddReactionsAsync(reactionsToAdd.ToArray()).ConfigureAwait(false);
+                    await newMessage.AddReactionsAsync(reactionsToAdd.ToArray()).ConfigureAwait(false);
                     await message.DeleteAsync().ConfigureAwait(false);
                 }).ConfigureAwait(false);
             }
 
             return;
+        }
+
+        private bool NeedsWarning(IEmbed embed)
+        {
+            if (embed.Title.Contains(OracleResources.OracleResult)) return true;
+
+            return false;
         }
     }
 }
