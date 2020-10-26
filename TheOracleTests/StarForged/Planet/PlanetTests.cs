779e343d1b5e3dc5c9cf42e02b27bee6c07195eb
Dominic Eliot <eliotd@gmail.com>
2020-10-26 11:23:28 -0700

    Adds brute force planet unit test

---
+++ TheOracleTests/StarForged/Planet/PlanetTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TheOracle.StarForged;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using TheOracle.Core;
+
+namespace TheOracle.StarForged.Tests
+{
+    [TestClass()]
+    public class PlanetTests
+    {
+        [TestMethod()]
+        public void VitalWorldTest()
+        {
+            var services = new ServiceCollection().AddSingleton<OracleService>().BuildServiceProvider();
+            Planet vitalWorld = null;
+
+            //find a vital world
+            for (int i = 0; i < 1000; i++)
+            {
+                var planet = Planet.GeneratePlanet("P-" + i.ToString(), SpaceRegion.Expanse, services);
+
+                planet.GetEmbedBuilder();
+            }
+        }
+    }
+}
