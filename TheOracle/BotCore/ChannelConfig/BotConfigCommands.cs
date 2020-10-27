b5932484ce961adf0b78f5ebebb71d7149688abc
Dominic Eliot <eliotd@gmail.com>
2020-10-27 14:46:09 -0700

    Adds database and ChannelSettings class

---
+++ TheOracle/BotCore/ChannelConfig/BotConfigCommands.cs
@@ -0,0 +1,69 @@
+﻿using Discord;
+using Discord.Commands;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+
+namespace TheOracle.BotCore
+{
+    public class BotConfigCommands : ModuleBase<SocketCommandContext>
+    {
+        public BotConfigCommands(IServiceProvider services)
+        {
+            Services = services;
+        }
+
+        public IServiceProvider Services { get; }
+
+        //[Command("Language")]
+        //public async Task SetChannelLocalization(string name)
+        //{
+        //    await ReplyAsync("Saved.");
+        //}
+
+        [Command("SetDefaultGame", ignoreExtraArgs: true)]
+        [Summary("Sets the default game for a channel.")]
+        public async Task SetDefaultGame(GameName game)
+        {
+            using var db = new DiscordChannelContext();
+            var existingSettings = await db.ChannelSettings.FirstOrDefaultAsync(cs => cs.ChannelID == Context.Channel.Id);
+
+            if (existingSettings != null)
+            {
+                existingSettings.DefaultGame = game;
+                await db.SaveChangesAsync().ConfigureAwait(false);
+                await Context.Message.AddReactionAsync(new Emoji("🆗")).ConfigureAwait(false);
+                return;
+            }
+            else
+            {
+                ChannelSettings cs = new ChannelSettings
+                {
+                    ChannelID = Context.Channel.Id,
+                    DefaultGame = game
+                };
+                db.ChannelSettings.Add(cs);
+                await db.SaveChangesAsync().ConfigureAwait(false);
+                await Context.Message.AddReactionAsync(new Emoji("🆗")).ConfigureAwait(false);
+            }
+        }
+
+        [Command("GetDefaultGame", ignoreExtraArgs: true)]
+        [Summary("Posts the default game for the channel.")]
+        public async Task GetDefaultGame()
+        {
+            using var db = new DiscordChannelContext();
+            var existingSettings = await db.ChannelSettings.FirstOrDefaultAsync(cs => cs.ChannelID == Context.Channel.Id);
+
+            if (existingSettings != null)
+            {
+                await ReplyAsync($"Your default game is: {existingSettings.DefaultGame}").ConfigureAwait(false);
+            }
+            else
+            {
+                await ReplyAsync($"Your default game not set.").ConfigureAwait(false);
+            }
+        }
+    }
+}
---
+++ TheOracle/Migrations/20201027171014_Initial.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TheOracle.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ChannelSettings",
+                columns: table => new
+                {
+                    ChannelID = table.Column<ulong>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    DefaultGame = table.Column<int>(nullable: false),
+                    Culture = table.Column<string>(nullable: true),
+                    RerollDuplicates = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChannelSettings", x => x.ChannelID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ChannelSettings");
+        }
+    }
+}
