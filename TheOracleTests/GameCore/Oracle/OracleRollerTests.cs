94a84ae86bb6d5603a30622cbb2354246877c2ea
Dominic Eliot <eliotd@gmail.com>
2020-11-01 07:58:46 -0800

    brute force test for complex oracle rolls

--- TheOracleTests/GameCore/Oracle/OracleRollerTests.cs
+++ TheOracleTests/GameCore/Oracle/OracleRollerTests.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Linq;
 using TheOracle.Core;
 
 namespace TheOracle.GameCore.Oracle.Tests
@@ -17,6 +19,19 @@ namespace TheOracle.GameCore.Oracle.Tests
             oracleService.RandomRow("Theme", GameName.Ironsworn);
 
             Assert.ThrowsException<ArgumentException>(() => oracleService.RandomRow("Action"));
+        }
+
+        [TestMethod()]
+        public void BuildRollResultsTest1()
+        {
+            var services = new ServiceCollection().AddSingleton<OracleService>().BuildServiceProvider();
+
+            for (int i = 0; i < 100; i++)
+            {
+                var rand = new Random(i);
+                var roller = new OracleRoller(services, GameName.Starforged, rand).BuildRollResults("sso");
+                Console.WriteLine(string.Join(", ", roller.RollResultList.Select(rr => rr.Result.Description)));
+            }
         }
     }
 }
