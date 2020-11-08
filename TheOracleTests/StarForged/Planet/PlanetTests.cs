e10a3d47f52dd67cffe3d11fb61fb1bd6fa9df6c
Dominic Eliot <eliotd@gmail.com>
2020-11-07 18:29:40 -0800

    fix for new planet gen parm

--- TheOracleTests/StarForged/Planet/PlanetTests.cs
+++ TheOracleTests/StarForged/Planet/PlanetTests.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.Extensions.DependencyInjection;
 using TheOracle.Core;
+using TheOracle.StarForged.Planets;
 
 namespace TheOracle.StarForged.Tests
 {
@@ -20,7 +21,7 @@ namespace TheOracle.StarForged.Tests
             //find a vital world
             for (int i = 0; i < 1000; i++)
             {
-                var planet = Planet.GeneratePlanet("P-" + i.ToString(), SpaceRegion.Expanse, services);
+                var planet = Planet.GeneratePlanet("P-" + i.ToString(), SpaceRegion.Expanse, services, 0);
 
                 planet.GetEmbedBuilder();
             }
