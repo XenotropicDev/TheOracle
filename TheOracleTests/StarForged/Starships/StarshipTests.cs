7b7365422540a86f62973ba6f25b4ebfe57d3c8a
Dominic Eliot <eliotd@gmail.com>
2020-11-10 14:40:39 -0800

    brute force starship test

---
+++ TheOracleTests/StarForged/Starships/StarshipTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TheOracle.StarForged.Starships;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using TheOracle.Core;
+
+namespace TheOracle.StarForged.Starships.Tests
+{
+    [TestClass()]
+    public class StarshipTests
+    {
+        [TestMethod()]
+        public void GenerateShipTest()
+        {
+            var services = new ServiceCollection().AddSingleton<OracleService>().BuildServiceProvider();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                try
+                {
+                    var ship = Starship.GenerateShip(services, SpaceRegion.Outlands, $"ship-{i}", 0);
+
+                    ship.GetEmbedBuilder();
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine(i);
+                    throw;
+                }
+            }
+        }
+    }
+}
