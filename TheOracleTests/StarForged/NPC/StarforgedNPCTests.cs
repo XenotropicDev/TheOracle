0a6a8fafc4226ad313f0f7c82f0a6ea1d760f46c
Xeno <eliotd@gmail.com>
2020-11-27 15:31:31 -0800

    Star NPC unit test

---
+++ TheOracleTests/StarForged/NPC/StarforgedNPCTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TheOracle.StarForged.NPC;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using TheOracle.GameCore.Oracle;
+using System.Linq;
+
+namespace TheOracle.StarForged.NPC.Tests
+{
+    [TestClass()]
+    public class StarforgedNPCTests
+    {
+        [TestMethod()]
+        public void NPCBuildTest()
+        {
+            var services = new ServiceCollection().AddSingleton(new OracleService().Load()).BuildServiceProvider();
+            var NPC = new StarforgedNPC(services);
+
+            for (int i = 0; i < 100; i++)
+            {
+                NPC.Build("");
+
+                if (NPC.Goals.Any(g => g.Contains("[2x]"))) 
+                    Assert.Fail();
+            }
+        }
+    }
+}
