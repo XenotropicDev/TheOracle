7d3c1d59939d2f8639be6e497b8c134c7c58e79e
Xeno <eliotd@gmail.com>
2021-05-02 12:08:14 -0700

    meta: missing item for 2e9452df

--- TheOracle/StarForged/StarforgedUtilites.cs
+++ TheOracle/StarForged/StarforgedUtilites.cs
@@ -1,5 +1,7 @@
 ﻿using Discord;
 using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 using TheOracle.BotCore;
 using TheOracle.StarForged.Creatures;
 
@@ -13,6 +15,15 @@ namespace TheOracle.StarForged
             if (planetName.Contains("Outlands", StringComparison.OrdinalIgnoreCase)) return SpaceRegion.Outlands;
             if (planetName.Contains("Expanse", StringComparison.OrdinalIgnoreCase)) return SpaceRegion.Expanse;
             return SpaceRegion.None;
+        }
+
+        public static string GetAnyPlanetLocation(string value)
+        {
+            if (value.Contains("Planetside", StringComparison.OrdinalIgnoreCase)) return "Planetside";
+            if (value.Contains("Orbital", StringComparison.OrdinalIgnoreCase)) return "Orbital";
+            if (value.Contains("Deep Space", StringComparison.OrdinalIgnoreCase)) return "Deep Space";
+            if (value.Contains("Deepspace", StringComparison.OrdinalIgnoreCase)) return "Deep Space";
+            return null;
         }
 
         public static SpaceRegion SpaceRegionFromEmote(string reactionName)
@@ -41,6 +52,28 @@ namespace TheOracle.StarForged
             if (GenericReactions.fourEmoji.IsSameAs(reactionName)) return CreatureEnvironment.Liquid;
             if (GenericReactions.fiveEmoji.IsSameAs(reactionName)) return CreatureEnvironment.Air;
             return CreatureEnvironment.None;
+        }
+
+        /// <summary>
+        /// Returns the value of any settlement locations contained in a string, and an empty string if not.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="removeSettlement"></param>
+        /// <returns></returns>
+        public static string ExtractAnySettlementLocation(ref string value, bool removeSettlement = true)
+        {
+            string[] Locations = new string[] { "Planetside", "Orbital", "Deep space" };
+            var temp = value;
+
+            var SettlementLocation = Locations.FirstOrDefault(loc => temp.Contains(loc, StringComparison.OrdinalIgnoreCase)) ?? string.Empty;
+
+            if (SettlementLocation.Length > 0 && removeSettlement)
+            {
+                value = value.Replace(SettlementLocation, "", StringComparison.OrdinalIgnoreCase).Trim();
+                value = Regex.Replace(value, "  +", " ");
+            }
+
+            return SettlementLocation;
         }
     }
 }
