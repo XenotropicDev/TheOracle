d6a4afbafe5ab367a76072a9e8f566db546ddfca
Xeno <eliotd@gmail.com>
2021-08-12 15:00:27 -0700

    feature: Adds `!MovePost` command to move posts between channels

--- TheOracle/BotCore/GenericCommands.cs
+++ TheOracle/BotCore/GenericCommands.cs
@@ -1,6 +1,7 @@
 ﻿using Discord;
 using Discord.Addons.Interactive;
 using Discord.Commands;
+using Discord.WebSocket;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -133,6 +134,55 @@ namespace TheOracle.BotCore
             builder.AddField(FieldName, FieldValue, true);
 
             await message.ModifyAsync(msg => msg.Embed = builder.Build()).ConfigureAwait(false);
+        }
+
+        [Command("MovePost", RunMode = RunMode.Async)]
+        [Alias("MoveMessage")]
+        [Summary("Uses inline replies to move a message from one channel to another channel on the server. Make sure to Tag (#channel-name) the channel you want.")]
+        [Remarks("")]
+        public async Task MoveMessage(IGuildChannel mentionedChannel)
+        {
+            var message = Context.Message.ReferencedMessage;
+            if (message == null || message.Author.Id != Context.Client.CurrentUser.Id)
+            {
+                await ReplyAsync($"I can only move my own messages.").ConfigureAwait(false);
+                return;
+            }
+
+
+            if (mentionedChannel == Context.Channel) return;
+            if (!(mentionedChannel is ITextChannel))
+            {
+                await ReplyAsync("Mentioned channel must be a text channel");
+                return;
+            }
+
+            var user = await mentionedChannel.GetUserAsync(Context.Client.CurrentUser.Id);
+            var permissions = user?.GetPermissions(mentionedChannel);
+            if (permissions == null || !permissions.HasValue || !permissions.Value.SendMessages || !permissions.Value.AddReactions)
+            {
+                await ReplyAsync("I don't have permissions for that channel.");
+                return;
+            }
+
+            var newMsg = await (mentionedChannel as IMessageChannel).SendMessageAsync(message.Content, embed: message.Embeds.FirstOrDefault().ToEmbedBuilder().Build());
+
+            await Task.Run(async () => { 
+                var fetchedMessage = await Context.Channel.GetMessageAsync(message.Id); //This is to get around a bug in discord.net
+                var reactionsToAdd = fetchedMessage.Reactions.Where(item => item.Value.IsMe).Select(item => item.Key);
+                foreach (var reaction in reactionsToAdd)
+                {
+                    await newMsg.AddReactionAsync(reaction);
+                    await Task.Delay(300); //Manual delay to avoid the rate limiter
+                }
+                
+                if (message.IsPinned)
+                {
+                    await newMsg.PinAsync();
+                }
+                
+                await message.DeleteAsync();
+            }).ConfigureAwait(false);
         }
 
         [Command("MakeEmbed")]
