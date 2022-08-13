898178e897acf9900b5339ca55f8b1e228598169
Xeno <eliotd@gmail.com>
2022-08-12 20:12:57 -0700

    fix: issue with NpgSQL and case insensitive search

--- Server/Interactions/CharacterAutocomplete.cs
+++ Server/Interactions/CharacterAutocomplete.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text.RegularExpressions;
 using Discord.Interactions;
+using Microsoft.EntityFrameworkCore;
 using Server.DiscordServer;
 using TheOracle2.UserContent;
 
@@ -7,6 +8,7 @@ namespace TheOracle2;
 
 public class CharacterAutocomplete : AutocompleteHandler
 {
+    private readonly string likeFormatter = "%{0}%";
     public override Task<AutocompletionResult> GenerateSuggestionsAsync(IInteractionContext context, IAutocompleteInteraction autocompleteInteraction, IParameterInfo parameter, IServiceProvider services)
     {
         var db = services.GetRequiredService<ApplicationContext>();
@@ -23,7 +25,7 @@ public class CharacterAutocomplete : AutocompleteHandler
                 case > 0:
                     {
                         successList = db.PlayerCharacters
-                            .Where((pc) => pc.DiscordGuildId == guildId && pc.Name.Contains(userText, StringComparison.OrdinalIgnoreCase))
+                            .Where((pc) => pc.DiscordGuildId == guildId && EF.Functions.ILike(pc.Name, string.Format(likeFormatter, userText)))
                             .OrderBy(pc => pc.UserId != userId).ThenBy(pc => pc.Name)
                             .Take(SelectMenuBuilder.MaxOptionCount)
                             .Select(pc => new AutocompleteResult(pc.Name, pc.Id.ToString())).AsEnumerable();
