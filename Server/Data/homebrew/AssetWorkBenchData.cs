78b1a20388d32271bcca804654df700a2c4bb5d8
Dominic Eliot <DEliot@DMV.nv.gov>
2023-03-02 15:49:16 -0800

    feature: asset work bench adapter for asset importer

---
+++ Server/Data/homebrew/AssetWorkBenchData.cs
@@ -0,0 +1,37 @@
+﻿namespace TheOracle2.Data.AssetWorkbench;
+
+public class AssetWorkBenchData
+{
+    public int? documentFormatVersion { get; set; }
+    public string type { get; set; }
+    public string name { get; set; }
+    public string? writeIn { get; set; }
+    public int? track { get; set; }
+    public string? description { get; set; }
+    public Ability[] abilities { get; set; }
+    public Fonts fonts { get; set; }
+    public Icon? icon { get; set; }
+    public string? writeIn2 { get; set; }
+}
+
+public class Fonts
+{
+    public string assetTypeFontSize { get; set; }
+    public string assetNameFontSize { get; set; }
+    public string detailsFontSize { get; set; }
+    public string trackFontSize { get; set; }
+}
+
+public class Icon
+{
+    public string? name { get; set; }
+    public string? author { get; set; }
+    public string? dataUri { get; set; }
+}
+
+public class Ability
+{
+    public bool filled { get; set; }
+    public string text { get; set; }
+    public string name { get; set; }
+}
---
+++ Server/Data/homebrew/AssetWorkbenchAdapter.cs
@@ -0,0 +1,39 @@
+﻿namespace TheOracle2.Data.AssetWorkbench;
+
+internal class AssetWorkbenchAdapter : Asset
+{
+    public AssetWorkbenchAdapter(string jsonData)
+    {
+        WorkbenchData = JsonConvert.DeserializeObject<AssetWorkBenchData>(jsonData);
+        Convert();
+    }
+
+    public AssetWorkbenchAdapter(AssetWorkBenchData data)
+    {
+        WorkbenchData = data;
+        Convert();
+    }
+
+    public AssetWorkBenchData WorkbenchData { get; }
+
+    private Asset Convert()
+    {
+        AssetType = WorkbenchData.type;
+        Name = WorkbenchData.name;
+        Display = new Display { Title = WorkbenchData.name, Icon = WorkbenchData.icon?.dataUri };
+        if (WorkbenchData.track != null) ConditionMeter = new ConditionMeter { Min = 0, Value = WorkbenchData.track ?? 0, Max = WorkbenchData.track ?? 0};
+        Abilities = WorkbenchData.abilities
+            .Select(ability => new TheOracle2.Data.Ability
+            {
+                Text = ability.text,
+                Name = ability.name,
+                Enabled = ability.filled
+            })
+            .ToList();
+
+        if (!string.IsNullOrWhiteSpace(WorkbenchData.writeIn)) Inputs.Add(new() { InputType = AssetInput.Text, Name = WorkbenchData.writeIn, Adjustable = true });
+        if (!string.IsNullOrWhiteSpace(WorkbenchData.writeIn2)) Inputs.Add(new() { InputType = AssetInput.Text, Name = WorkbenchData.writeIn2, Adjustable = true });
+
+        return this;
+    }
+}
