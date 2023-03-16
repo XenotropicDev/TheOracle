a47abd35685df8df19ef484484caebef547a4597
XenotropicDev <eliotd@gmail.com>
2023-03-16 16:55:11 -0700

    tests: unit test for Asset Workbench adapter

---
+++ ServerTests/Data/homebrew/AssetWorkbenchAdapterTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TheOracle2.Data.AssetWorkbench;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TheOracle2.Data.AssetWorkbench.Tests
+{
+    [TestClass()]
+    public class AssetWorkbenchAdapterTests
+    {
+        [TestMethod()]
+        public void AssetWorkbenchAdapterTest()
+        {
+            var fileJson = File.ReadAllText(Path.Combine("Data", "WorkbenchAssetSample.json"));
+
+            Asset dsAsset = new AssetWorkbenchAdapter(fileJson);
+
+            Assert.IsNotNull(dsAsset);
+            Assert.AreEqual("Lightbearer", dsAsset.Name);
+        }
+    }
+}
