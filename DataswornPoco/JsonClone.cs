477ae4526b42f7b8332f799b0d3071ab0848b265
XenotropicDev <eliotd@gmail.com>
2023-03-28 13:15:25 -0700

    feature: adds a generic json object cloner

---
+++ DataswornPoco/JsonClone.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace DataswornPoco
+{
+    public static class JsonClone
+    {
+        /// <summary>
+        /// Perform a deep Copy of the object, using Json as a serialization method. NOTE: Private members are not cloned using this method.
+        /// </summary>
+        /// <typeparam name="T">The type of object being copied.</typeparam>
+        /// <param name="source">The object instance to copy.</param>
+        /// <returns>The copied object.</returns>
+        public static T? CloneJson<T>(this T source)
+        {
+            // Don't serialize a null object, simply return the default for that object
+            if (ReferenceEquals(source, null)) return default;
+
+            var deserializeSettings = new JsonSerializerSettings { ObjectCreationHandling = ObjectCreationHandling.Replace };
+
+            return JsonConvert.DeserializeObject<T>(JsonConvert.SerializeObject(source), deserializeSettings);
+        }
+    }
+}
