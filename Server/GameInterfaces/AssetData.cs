b77be2f21039db538702ad6c71ccb01374af3e7f
XenotropicDev <eliotd@gmail.com>
2023-05-09 07:02:49 -0700

    fix: assetdata using ef core navigation for Asset property

--- Server/GameInterfaces/AssetData.cs
+++ Server/GameInterfaces/AssetData.cs
@@ -11,7 +11,7 @@ public record AssetData
 
     public AssetData(Asset asset, ulong id)
     {
-        AssetId = asset.JsonId;
+        AssetId = asset.Id;
         SelectedAbilities = asset.Abilities.Where(a => a.Enabled).Select(a => a.JsonId).ToList();
         CreatorDiscordId = id;
         if (asset.ConditionMeter?.Value > 0)
@@ -22,7 +22,8 @@ public record AssetData
 
     public int Id { get; set; }
     public ulong CreatorDiscordId { get; set; }
-    public string AssetId { get; set; } = String.Empty;
+    public uint AssetId { get; set; }
+    public virtual Asset Asset { get; set; }
     public IList<string> SelectedAbilities { get; set; } = new List<string>();
     public IList<string> Inputs { get; set; } = new List<string>();
     public int ConditionValue { get; set; }
